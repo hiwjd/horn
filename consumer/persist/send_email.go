b36bcb56d7b6bd1c062fadbc758207f1acb82fee
hiwjd <self@hiwjd.com>
2016-11-26 23:14:16 +0800

    注册发送邮件topic改为发送邮件topic，同时增加发送找回密码的处理

--- consumer/persist/send_email.go
+++ consumer/persist/send_email.go
@@ -46,6 +46,23 @@ func signupEmailProcesser(handler *Handler, body []byte) error {
 			FromName: "",
 			ReplyTo:  "",
 		}
+	case "find_pass":
+		token, err := genFindPassToken(handler.mysqlManager, v.Email)
+		if err != nil {
+			return err
+		}
+		link := fmt.Sprintf("http://app.horn.com:9092/api/find_pass/reset?s=%s", token)
+
+		template := `<p>找回密码</p><p>我们收到了找回密码的请求，如果这不是您操作的，请忽略。</p><p>点击该链接重置密码<a target="_blank" href="%s">%s</p><p>HONR团队</p>`
+		html := fmt.Sprintf(template, link, link)
+		data = &sendcloud.EmailData{
+			From:     "support@horn.com",
+			To:       v.Email,
+			Subject:  "找回密码",
+			Html:     html,
+			FromName: "",
+			ReplyTo:  "",
+		}
 	default:
 		return errors.New("不支持的邮件发送类型")
 	}
@@ -57,6 +74,33 @@ func signupEmailProcesser(handler *Handler, body []byte) error {
 	}
 
 	return nil
+}
+
+func genFindPassToken(mysqlManager *mysql.Manager, email string) (string, error) {
+	db, err := mysqlManager.Get("write")
+	if err != nil {
+		log.Printf(" -> 获取数据库连接失败: %s \r\n", err.Error())
+		return "", err
+	}
+
+	token := utils.RandString(57)
+	expires_at := time.Now().Unix()
+
+	sql := `
+        INSERT INTO find_pass_email
+            (email,token,count,state,expires_at)
+        VALUES
+            (?,?,1,?,?) 
+        ON DUPLICATE KEY UPDATE 
+            token=?, count=count+1, state=?, expires_at=?
+    `
+	_, err = db.Exec(sql, email, token, "valid", expires_at, token, "valid", expires_at)
+	if err != nil {
+		log.Printf(" -> 新增／更新find_pass_email出错: %s \r\n", err.Error())
+		return "", err
+	}
+
+	return token, nil
 }
 
 func genSignupToken(mysqlManager *mysql.Manager, email string) (string, error) {
@@ -71,13 +115,13 @@ func genSignupToken(mysqlManager *mysql.Manager, email string) (string, error) {
 
 	sql := `
         INSERT INTO signup_email
-            (email,token,count,expires_at)
+            (email,token,count,state,expires_at)
         VALUES
-            (?,?,1,?) 
+            (?,?,1,?,?) 
         ON DUPLICATE KEY UPDATE 
-            token=?, count=count+1, expires_at=?
+            token=?, count=count+1, state=?, expires_at=?
     `
-	_, err = db.Exec(sql, email, token, expires_at, token, expires_at)
+	_, err = db.Exec(sql, email, token, "valid", expires_at, token, "valid", expires_at)
 	if err != nil {
 		log.Printf(" -> 新增／更新signup_email出错: %s \r\n", err.Error())
 		return "", err
